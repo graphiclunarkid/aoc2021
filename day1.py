6ba6dd0acc779e02bbe124e4cf88435f05d561bb
Richard King <richard@richardskingdom.net>
2021-12-01 14:36:12 +0000

    Solution to part 1

--- day1.py
+++ day1.py
@@ -2,9 +2,14 @@ input = open('input', 'r')
 
 depths = []
 for line in input:
-    depths.append(line[:len(line)-1])
+    depths.append(int(line[:len(line)-1]))
 
 input.close()
 
-print(depths)
+count = 0
 
+for i in range(1,len(depths)):
+    if depths[i] > depths[i-1]:
+        count += 1
+
+print(count)
