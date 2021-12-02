12b665acdcbd39191506ef7c25f055b62d014495
Richard King <richard@richardskingdom.net>
2021-12-02 20:44:02 +0000

    Use Python 3 f-strings for output formatting

--- sonarsweep.py
+++ sonarsweep.py
@@ -9,5 +9,5 @@ def smooth(granular):
 def count(readings):
     return(len([i for i in range(1,len(readings)) if readings[i] > readings[i-1]]))
 
-print("Count: {}").format(count(readings))
-print("Noise-reduced count: {}").format(count(smooth(readings)))
+print(f"Count: {count(readings)}")
+print(f"Noise-reduced count: {count(smooth(readings))}")
