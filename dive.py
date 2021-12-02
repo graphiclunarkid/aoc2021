214904a169b04109a214052bec1bd4c3bb77004d
Richard King <richard@richardskingdom.net>
2021-12-02 21:47:48 +0000

    Solution to part 1

---
+++ dive.py
@@ -0,0 +1,15 @@
+with open('day2-input', 'r') as input:
+    commands = []
+    for line in input:
+        commands.append(line[:len(line)-1])
+
+directions = [command.split(' ') for command in commands]
+
+horiz = sum([int(value) for direction,value in directions if direction == "forward"])
+down  = sum([int(value) for direction,value in directions if direction == "down"])
+up    = sum([int(value) for direction,value in directions if direction == "up"])
+depth = down - up
+
+print(f"Horizontal position: {horiz}")
+print(f"Depth: {depth}")
+print(f"Product: {horiz * depth}")
