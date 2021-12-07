aa316700c59ecf7364331f1a50b5d3072dea788f
Richard King <richard@richardskingdom.net>
2021-12-07 20:36:07 +0000

    Solution to part 1

--- day5.py
+++ day5.py
@@ -1,5 +1,32 @@
 from collections import Counter
 
+def expand(points):
+    start, end = points
+    startx, starty = start
+    endx, endy = end
+
+    startx = int(startx)
+    starty = int(starty)
+    endx = int(endx)
+    endy = int(endy)
+
+    line = []
+    if startx == endx:
+        if starty < endy:
+            line = [(startx, y) for y in range(starty,endy + 1)]
+        else:
+            line = [(startx, y) for y in range(endy,starty + 1)]
+    elif starty == endy:
+        if startx < endx:
+            line = [(x, starty) for x in range(startx,endx + 1)]
+        else:
+            line = [(x, starty) for x in range(endx,startx + 1)]
+    else:
+        raise ValueError
+
+    return(line)
+
+
 with open('inputs/day5-input', 'r') as input:
     inputLines = []
     for line in input:
@@ -8,16 +35,25 @@ with open('inputs/day5-input', 'r') as input:
 
 allLines = []
 for line in inputLines:
-    allLines.append([i.split(',') for i in line])
+    allLines.append([tuple(i.split(',')) for i in line])
 
-horizLines, vertLines, diagLines = []
+horizLines = []
+vertLines = []
+diagLines = []
+ventMap = []
 for line in allLines:
     if line[0][1] == line[1][1]:
         """ Horizontal """
         horizLines.append(line)
+        ventMap.extend(expand(line))
     elif line[0][0] == line[1][0]:
         """ Vertical """
         vertLines.append(line)
+        ventMap.extend(expand(line))
     else:
         """ Diagonal """
         diagLines.append(line)
+
+hotSpots = Counter(ventMap)
+hotSpots = {key:val for key, val in hotSpots.items() if val > 1}
+print(f"{len(hotSpots)}")
