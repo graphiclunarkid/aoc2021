8630b03cc55e5213373bc16d70a22da06203ce19
Richard King <richard@richardskingdom.net>
2021-12-09 22:02:31 +0000

    Solution to both parts

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,22 +3,44 @@ use std::io::prelude::*;
 use std::path::Path;
 use counter::Counter;
 
-fn increment_day(s: counter::Counter<&str>) -> counter::Counter<&str> {
+fn increment_day(school: counter::Counter<&str>) -> counter::Counter<&str> {
 
-    let mut new_school = s.clone();
+    let mut new_school = Counter::new();
 
-    new_school[&"7"] = s[&"8"];
-    new_school[&"6"] = s[&"7"];
-    new_school[&"5"] = s[&"6"];
-    new_school[&"4"] = s[&"5"];
-    new_school[&"3"] = s[&"4"];
-    new_school[&"2"] = s[&"3"];
-    new_school[&"1"] = s[&"2"];
-    new_school[&"0"] = s[&"1"];
-    new_school[&"6"] += s[&"0"];    // Fish on timer 0 reset to timer 6
-    new_school[&"8"] += s[&"0"];    // Fish on timer 0 also produce new fish on timer 8
+    new_school[&"7"] = school[&"8"];    // Each number > 0 decreases by 1 if it was
+    new_school[&"6"] = school[&"7"];    // present at the start of the day
+    new_school[&"5"] = school[&"6"];
+    new_school[&"4"] = school[&"5"];
+    new_school[&"3"] = school[&"4"];
+    new_school[&"2"] = school[&"3"];
+    new_school[&"1"] = school[&"2"];
+    new_school[&"0"] = school[&"1"];
+    new_school[&"6"] += school[&"0"];    // Each day, each 0 becomes a 6
+    new_school[&"8"] += school[&"0"];    // and adds a new 8 to the school
 
     new_school
+
+}
+
+fn simulate_lanternfish(days: u32, mut school: counter::Counter<&str>) {
+
+    println!("");
+    println!("Initial state: {:?}", school);
+
+    for _ in 1..days+1 {
+
+        school = increment_day(school);
+
+    }
+
+    let mut count = 0;
+    for value in school.values() {
+        count += value;
+    }
+
+    println!("State after {} days: {:?}", days, school);
+    println!("Total number of lanternfish after {} days: {:?}", days, count);
+    
 }
 
 fn main() {
@@ -40,10 +62,9 @@ fn main() {
 
     // Convert puzzle input into a Counter object (https://docs.rs/counter/latest/counter/)
     let school: Counter<&str> = input.trim().split(',').collect();
+   
+    // Execute simulation loop
+    simulate_lanternfish(80, school.clone());
+    simulate_lanternfish(256, school.clone());
 
-    println!("   Today's school: {:?}", school);
-
-    let new_school = increment_day(school);
-
-    println!("Tomorrow's school: {:?}", new_school);
 }
