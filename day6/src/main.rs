c05fd0a79c2653164714fb7775b521fd044a0b14
Richard King <richard@richardskingdom.net>
2021-12-09 20:10:39 +0000

    Logic to increment fish population by 1 day

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -4,8 +4,21 @@ use std::path::Path;
 use counter::Counter;
 
 fn increment_day(s: counter::Counter<&str>) -> counter::Counter<&str> {
-    // We'll implement the logic here
-    s
+
+    let mut new_school = s.clone();
+
+    new_school[&"7"] = s[&"8"];
+    new_school[&"6"] = s[&"7"];
+    new_school[&"5"] = s[&"6"];
+    new_school[&"4"] = s[&"5"];
+    new_school[&"3"] = s[&"4"];
+    new_school[&"2"] = s[&"3"];
+    new_school[&"1"] = s[&"2"];
+    new_school[&"0"] = s[&"1"];
+    new_school[&"6"] += s[&"0"];    // Fish on timer 0 reset to timer 6
+    new_school[&"8"] += s[&"0"];    // Fish on timer 0 also produce new fish on timer 8
+
+    new_school
 }
 
 fn main() {
