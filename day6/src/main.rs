fbaa295fd78ff7e5d224aa117bfa0d10faa028db
Richard King <richard@richardskingdom.net>
2021-12-09 13:56:32 +0000

    Better way of reading a file

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 use counter::Counter;
 
 fn increment_day(s: counter::Counter<&str>) -> counter::Counter<&str> {
@@ -7,13 +10,23 @@ fn increment_day(s: counter::Counter<&str>) -> counter::Counter<&str> {
 
 fn main() {
 
-    // Read puzzle input from file
-    let input = include_str!("day6-input");
+    // Open puzzle input file
+    let path = Path::new("data/day6-input");
+    let display = path.display();
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("Couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
 
-    // Convert input into a Counter object I don't understand <Counter<_>>() yet: I just stole it
-    // from https://docs.rs/counter/latest/counter/
-    let school = input.trim().split(',')
-        .collect::<Counter<_>>();
+    // Read puzzle input from file
+    let mut input = String::new();
+    match file.read_to_string(&mut input) {
+        Err(why) => panic!("Couldn't read {}: {}", display, why),
+        Ok(input) => input,
+    };
+
+    // Convert puzzle input into a Counter object (https://docs.rs/counter/latest/counter/)
+    let school: Counter<&str> = input.trim().split(',').collect();
 
     println!("   Today's school: {:?}", school);
 
