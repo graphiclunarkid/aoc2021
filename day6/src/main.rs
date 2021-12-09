75d5c280f64b3fb5f229186a56152f5ef0de34fa
Richard King <richard@richardskingdom.net>
2021-12-09 00:37:28 +0000

    Fixed? Compiles and prints correct result but not sure about effect on variables...

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -3,7 +3,7 @@ use counter::Counter;
 // Extremely n00b problem #1: all the example functions I can find use standard data types like i32
 // or char.  This fails to compile because I can't figure out how to reference the data type of a
 // Counter object (struct?) in the function declaration...
-fn increment_day(s: Counter<&str>) -> Counter<&str> {
+fn increment_day(s: counter::Counter<&str>) -> counter::Counter<&str> {
     s
 }
 
@@ -23,9 +23,9 @@ fn main() {
 
     print_type_of(&school);
     
-    let new_school = increment_day(&school);
+    let new_school = increment_day(school);
 
-    println!("   Today's school: {:?}", school);
+//    println!("   Today's school: {:?}", school);
 
     println!("Tomorrow's school: {:?}", new_school);
 }
